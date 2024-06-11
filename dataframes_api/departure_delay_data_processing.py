083842f82087396f9b60606056777bc3864fc70a
Anand Satheesh <anands282@gmail.com>
2024-06-11 15:52:20 +0530

    updated query in spark sql

--- dataframes_api/departure_delay_data_processing.py
+++ dataframes_api/departure_delay_data_processing.py
@@ -9,4 +9,8 @@ df.show()
 df.createOrReplaceTempView("us_delay_flights_tbl")
 
 df2 = spark.sql("""select * from us_delay_flights_tbl where delay > 50 """)
+df3 = spark.sql("""select date, delay, origin, destination FROM us_delay_flights_tbl WHERE delay > 120 AND origin = 'SFO' and 
+destination = 'ORD' order by delay desc""")
 df2.show()
+df3.show()
+
